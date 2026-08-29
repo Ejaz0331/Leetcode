576fd855a21f78943ccdd42e0913c77d40deaa1b
Mohammad Ejaz M <ejaz031206@gmail.com>
2026-08-29 11:30:33 +0530

    Time: 16 ms (52.88%), Space: 52.7 MB (92%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] color = new int[n+1];
+        for(int i=0; i<=n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] temp: dislikes){
+            int from = temp[0];
+            int to = temp[1];
+            adj.get(from).add(to);
+            adj.get(to).add(from);
+        }
+        for(int i=1; i<=n; i++){
+            color[i] = -1;
+        }
+        for(int i=1; i<=n; i++){
+            if(color[i] == -1){
+                if(dfsPartition(i, dislikes, color, adj, 1) == false) return false;
+            }
+        }
+        return true;
+    }
+    private boolean dfsPartition(int node, int[][] dislikes, int[] color, List<List<Integer>> adj, int col){
+        color[node] = col;
+        for(int it: adj.get(node)){
+            if(color[it] == -1){
+                if(dfsPartition(it, dislikes, color, adj, 1-color[node]) == false) return false;
+            }
+            else if(color[it] == color[node]) return false;
+        }
+        return true;
+    }
+}
