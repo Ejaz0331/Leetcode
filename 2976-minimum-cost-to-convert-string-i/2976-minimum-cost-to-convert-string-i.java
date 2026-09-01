338f8ca434131ef056f5812b5526f6a59ec82477
Mohammad Ejaz M <ejaz031206@gmail.com>
2026-09-01 11:33:32 +0530

    Time: 23 ms (56.72%), Space: 48 MB (64.59%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        long[][] dist = new long[26][26];
+        long INF = (long) 1e14;
+
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < original.length; i++) {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            dist[u][v] = Math.min(dist[u][v], (long) cost[i]);
+        }
+
+        for (int k = 0; k < 26; k++) {
+            for (int i = 0; i < 26; i++) {
+                for (int j = 0; j < 26; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        long totalCost = 0;
+        for (int i = 0; i < source.length(); i++) {
+            int u = source.charAt(i) - 'a';
+            int v = target.charAt(i) - 'a';
+            if (dist[u][v] >= INF) {
+                return -1;
+            }
+            totalCost += dist[u][v];
+        }
+
+        return totalCost;
+    }
+}
