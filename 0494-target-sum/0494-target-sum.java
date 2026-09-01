4361fd7232497225eb6f77e835b660b732660659
Mohammad Ejaz M <ejaz031206@gmail.com>
2026-09-01 22:33:37 +0530

    Time: 2 ms (99.96%), Space: 43.4 MB (54.86%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        if (Math.abs(target) > sum || (sum + target) % 2 != 0) {
+            return 0;
+        }
+        
+        int p = (sum + target) / 2;
+        int[] dp = new int[p + 1];
+        dp[0] = 1;
+        
+        for (int num : nums) {
+            for (int i = p; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+        
+        return dp[p];
+    }
+}
