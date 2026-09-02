e704172fc3c7c71b6e57c431bdd307361d6d511c
Mohammad Ejaz M <ejaz031206@gmail.com>
2026-09-02 23:03:01 +0530

    Time: 4 ms (97.3%), Space: 46.6 MB (73.83%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minTaps(int n, int[] ranges) {
+        int[] maxReach = new int[n + 1];
+        
+        for (int i = 0; i < ranges.length; i++) {
+            int left = Math.max(0, i - ranges[i]);
+            int right = Math.min(n, i + ranges[i]);
+            maxReach[left] = Math.max(maxReach[left], right);
+        }
+        
+        int taps = 0;
+        int currEnd = 0;
+        int nextEnd = 0;
+        
+        for (int i = 0; i <= n; i++) {
+            if (i > nextEnd) {
+                return -1;
+            }
+            if (i > currEnd) {
+                taps++;
+                currEnd = nextEnd;
+            }
+            nextEnd = Math.max(nextEnd, maxReach[i]);
+        }
+        
+        return taps;
+    }
+}
